f4885fd6b65fe20a20ac3665ab977d8d9902c13e
Anuja <anuja.kumar@medecision.com>
2021-01-22 15:50:28 +0530

    Initial commit

---
+++ src/main/java/com/crtbrkr/svca/ServiceAApplication.java
@@ -0,0 +1,24 @@
+package com.crtbrkr.svca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class ServiceAApplication {
+	static int counter=0;
+
+	@RequestMapping("/possible")
+	public String showPossibilities() throws Exception {
+		counter++;
+		if(counter%3 == 0)
+			throw new Exception();
+		return "Anuja_"+Math.random();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAApplication.class, args);
+	}
+
+}
